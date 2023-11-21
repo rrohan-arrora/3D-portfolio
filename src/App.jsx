08855cdc3db91828a9109370c5bfa8b82202898c
Aaksh Gadekar <an14092000@gmail.com>
2023-11-21 13:02:24 +0530

    Added components Imported all the components in the index file Exported the components from the index file in components section and directly called in the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import { BrowserRouter } from "react-router-dom";
+import {About, Contact, Experience, FeedBacks, Hero, Navbar, Tech, Works, StarsCanvas} from './components';
+
 const App = () => {
   return (
     <div>
