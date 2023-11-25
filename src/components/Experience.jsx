323d9b36a37d7c7337a98e2efa1cb0017ec28fbf
Aaksh Gadekar <an14092000@gmail.com>
2023-11-25 20:04:03 +0530

    Added changes in exp

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -33,7 +33,7 @@ const ExperienceCard = ({experience}) => {
       <div>
         <h3 className='text-white text-[24px] font-bold'>{experience.title}</h3>
         <p
-          className='text-primary text-orange-600 text-[16px] font-semibold'
+          className='text-lg text-orange-600 text-[16px] font-semibold'
           style={{ margin: 0 }}
         >
           {experience.company_name}
